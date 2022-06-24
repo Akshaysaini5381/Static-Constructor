1f7511f939a3d516f95812ef59a76b4ce3a95423
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-06-24 05:55:37 +0530

    Add project files.

---
+++ Static Constructor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Static_Constructor
+{
+    class Program
+    {
+        static int num = 100;
+        static Program()
+        {
+            num = 200;
+            Console.WriteLine("static constructor "+num);
+        }
+        static void Main(string[] args)
+        {
+            num = 300;
+            Console.WriteLine(num);
+            
+        }
+    }
+}
